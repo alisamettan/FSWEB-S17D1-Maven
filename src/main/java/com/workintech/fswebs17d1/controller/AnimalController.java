95260683115fb20bcf68e3ca71f8b0304ea55f20
alisamettan <alisamet_tan@hotmail.com>
2024-04-22 20:49:10 +0300

    Revised.

--- src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
+++ src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
@@ -1,42 +1,50 @@
 package com.workintech.fswebs17d1.controller;
 
 import com.workintech.fswebs17d1.entity.Animal;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@RequestMapping("/workintech/animal")
 @RestController
 public class AnimalController {
     private Map<Integer, Animal> animals=new HashMap<>();
 
-    @GetMapping("/workintech/animal")
+    @Value("${project.developer.fullname}")
+    private String fullName;
+
+    @Value("${course.name}")
+    private String courseName;
+
+
+
+
+    @GetMapping()
     public List<Animal> getAnimals(){
        return animals.values().stream().toList();
     }
 
-    @GetMapping("/workintech/animal/{id}")
+    @GetMapping("/{id}")
     public Animal getAnimal(@PathVariable int id){
-        if(!animals.containsKey(id)){
-           return null;
-        }
-        return animals.get(id);
+        return this.animals.get(id);
     }
 
-    @PostMapping("/workintech/animal")
+    @PostMapping()
     public Animal save(@RequestBody Animal animal){
         animals.put(animal.getId(),animal);
         return animal;
     }
 
-    @PutMapping("/workintech/animal/{id}")
+    @PutMapping("/{id}")
     public Animal update(@PathVariable int id,@RequestBody Animal animal){
-        animals.put(id,new Animal(id,animal.getName()));
+        animals.put(id,new Animal(id,animal.getName()));//animals.replace(id,animal)
         return animal;
     }
 
-    @DeleteMapping("/workintech/animal/{id}")
+    @DeleteMapping("/{id}")
     public Animal delete(@PathVariable int id){
         Animal animal=animals.get(id);
         animals.remove(id,animal);
